4601cb89ab7ee24ef0275ef9e14bc9789e354ff4
h573763 <573763@stud.hvl.no>
2022-08-22 10:02:04 +0200

    changes to the sample threads

--- Oblig/Oblig-1/src/hvl/dat108/dat108/Main.java
+++ Oblig/Oblig-1/src/hvl/dat108/dat108/Main.java
@@ -6,24 +6,28 @@ public class Main extends Thread{
     public static void main(String[] args) {
         System.out.println("This is not a thread");
 
-
+        //Starting 10 Threads
         for(int i = 0; i < 10; i++){
-            new Main().start();
-            try{
-                Thread.sleep(100);
-            }catch (InterruptedException e){
-                System.out.println(e.getMessage());
-            }
+            //Thread start
+            new Thread("Thread: " + i){
+                public void run(){
+                    System.out.println("This is ran from " + Thread.currentThread().getName());
+                    try{
+                        //Thread is set to sleep
+                        Thread.sleep(1000);
+                    }catch (InterruptedException e){
+                        //In the case of an emergency
+                        System.out.println(e.getMessage());
+                    }
+                    System.out.println("End of " + Thread.currentThread().getName());
+                }
+            }.start();
+            //Thread end
+
 
         }
-
+        //Call outside the threads
         System.out.println("this is also outside the thread");
-
-    }
-
-    @Override
-    public void run(){
-            System.out.println("This is called in thread " + Thread.currentThread().getName());
 
     }
 }
