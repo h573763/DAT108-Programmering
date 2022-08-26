58fc2a5d57f60d0ceed537bdd913318c3044c3b2
h573763 <573763@stud.hvl.no>
2022-08-26 12:27:38 +0200

    Legger til Threads for input og output

--- Oblig/Oblig-1/src/no/hvl/dat108/task1/Main.java
+++ Oblig/Oblig-1/src/no/hvl/dat108/task1/Main.java
@@ -4,7 +4,20 @@ public class Main extends Thread{
 
 
     public static void main(String[] args) {
-        
+
+        Thread input = new Thread("Input Thread"){
+            @Override
+            public void run(){
+                
+            }
+        };input.start();
+
+        Thread output = new Thread("Output Thread"){
+            @Override
+            public void run(){
+
+            }
+        };output.start();
     }
 
 
