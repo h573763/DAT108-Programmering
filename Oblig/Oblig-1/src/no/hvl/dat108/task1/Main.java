bce094b13d6f873f6bb17a345b60069073382fde
h573763 <573763@stud.hvl.no>
2022-08-26 13:19:52 +0200

    Oppgave 1 løst

--- Oblig/Oblig-1/src/no/hvl/dat108/task1/Main.java
+++ Oblig/Oblig-1/src/no/hvl/dat108/task1/Main.java
@@ -1,24 +1,49 @@
 package no.hvl.dat108.task1;
+import javax.swing.*;
 
-public class Main extends Thread{
 
+public class Main extends Thread {
+    private static String inString = "";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
 
-        Thread input = new Thread("Input Thread"){
+        String message = "Skriv noe morsomt Skriv quit for å avslutte";
+        Thread input = new Thread("Input Thread") {
             @Override
-            public void run(){
-                
+            public void run() {
+                while (true) {
+                    inString = JOptionPane.showInputDialog(message);
+                    if (inString.equals("quit")) {
+                        break;
+                    }
+                }
             }
-        };input.start();
+        };
+        input.start();
 
-        Thread output = new Thread("Output Thread"){
+        Thread output = new Thread("Output Thread") {
             @Override
-            public void run(){
-
+            public void run() {
+                while(true){
+                    if(inString.equals("quit")){
+                        break;
+                    }
+                    System.out.println(inString);
+                    try {
+                        Thread.sleep(3000);
+                    }catch (InterruptedException e){
+                        System.out.println(e.getMessage());
+                    }
+                }
             }
-        };output.start();
+
+        };
+        output.start();
+
+        input.join();
+        output.join();
+
+        System.out.println("Quiting...");
+
     }
-
-
 }
