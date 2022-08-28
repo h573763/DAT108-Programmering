68949cf1e16a8b7679f27abbb5af6b32866cf89a
westermin <reaverwester@gmail.com>
2022-08-28 17:03:35 +0200

    added HamburgerTray.java

--- Oblig/Oblig-1/src/no/hvl/dat108/task2/Main.java
+++ Oblig/Oblig-1/src/no/hvl/dat108/task2/Main.java
@@ -1,7 +1,24 @@
 package no.hvl.dat108.task2;
 
+import no.hvl.dat108.task2.dataobjects.HamburgerTray;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String... blablabla) {
+
+        final String[] kokker = {"Anne", "Erik", "Knut"};
+        final String[] servitorer = {"Mia", "Per"};
+        final int KAPASITET = 4;
+
+        skrivUtHeader(kokker, servitorer, KAPASITET);
+
+        HamburgerTray tray = new HamburgerTray(KAPASITET);
+
+        for (String navn : kokker) {
+            new Kokk(brett, navn).start();
+        }
+        for (String navn : servitorer) {
+            new Servitor(brett, navn).start();
+        }
 
     }
 }
