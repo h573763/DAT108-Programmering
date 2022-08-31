ebe20c72d940f1734840a7b1a6d7f2cf704e0457
h573763 <573763@stud.hvl.no>
2022-08-31 14:58:14 +0200

    mindre endringer til Cook.java, Service.java

--- Oblig/Oblig-1/src/no/hvl/dat108/task2/dataobjects/Cook.java
+++ Oblig/Oblig-1/src/no/hvl/dat108/task2/dataobjects/Cook.java
@@ -2,9 +2,7 @@ package no.hvl.dat108.task2.dataobjects;
 
 import no.hvl.dat108.task2.adt.Queue;
 
-import java.util.ArrayList;
-
-public class Cook{
+public class Cook {
     private String name;
     private Queue<Hamburger> burgerTray;
 
@@ -12,17 +10,13 @@ public class Cook{
         this.name = name;
         this.burgerTray = burgerTray;
     }
-    public synchronized void makeOrder(Hamburger burger){//kun en tråd kan legge til burger av gangen
 
-        if(burgerTray.size() < burgerTray.getLim()) {
-            burgerTray.enQueue(burger);//legger til burger i køen
-            System.out.println("Cook " + name + " is adding burger: " + burger.getId() + " traycount: " + burgerTray.size());
+    public synchronized void makeOrder(Hamburger burger) {//kun en tråd kan legge til burger av gangen
+        burgerTray.enQueue(burger);//legger til burger i køen
+        System.out.println("Cook " + name + " is adding burger: " + burger.getId() + " traycount: " + burgerTray.size());
 
-        }else{
-            System.out.println("The tray is full " + name + " cannot make a burger");
-
-        }
     }
+
     public String getName() {
         return name;
     }
--- Oblig/Oblig-1/src/no/hvl/dat108/task2/dataobjects/Service.java
+++ Oblig/Oblig-1/src/no/hvl/dat108/task2/dataobjects/Service.java
@@ -1,23 +1,19 @@
 package no.hvl.dat108.task2.dataobjects;
 
-import no.hvl.dat108.task2.Main;
 import no.hvl.dat108.task2.adt.Queue;
 
-public class Service extends Main {
+public class Service {
     private String name;
     private Queue<Hamburger> tray;
+
     public Service(String name, Queue<Hamburger> tray) {
         this.name = name;
         this.tray = tray;
     }
-    public synchronized void deliver(Hamburger burger){ //kun en servitør kan lever burger av gangen
 
-        if(!(tray.isEmpty())){ //så lenge køen ikke er tom
-            tray.deQueue(); //fjern øverste burger i fra køen
-            System.out.println("Server " + name + " is serving a burger: " + burger.getId() + " traycount: " + tray.size());
-        }else{
-            System.out.println("The tray is empty " + name + " cannot serve a burger");
-        }
+    public synchronized void deliver(Hamburger burger) { //kun en servitør kan lever burger av gangen
+        tray.deQueue(); //fjern øverste burger i fra køen
+        System.out.println("Server " + name + " is serving a burger: " + burger.getId() + " traycount: " + tray.size());
     }
 
 
