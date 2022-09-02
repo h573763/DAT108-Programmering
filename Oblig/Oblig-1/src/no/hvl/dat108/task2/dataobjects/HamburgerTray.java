874c9e9847f687271663b1cd3b17c4c7536d1890
h573763 <573763@stud.hvl.no>
2022-09-02 13:40:17 +0200

    Mindre endringer

--- Oblig/Oblig-1/src/no/hvl/dat108/task2/dataobjects/HamburgerTray.java
+++ Oblig/Oblig-1/src/no/hvl/dat108/task2/dataobjects/HamburgerTray.java
@@ -11,20 +11,12 @@ public class HamburgerTray{
 
     public synchronized void add(Hamburger burger){
 
-//        if(burgerTray.size() < trayMax){
         burgerTray.enQueue(burger);
         System.out.println("" + burger.getName());
         System.out.println(burgerTray.size());
-
-//        }else{//           System.out.println("The tray is full"); // senere legg stop
-//        }
     }
     public synchronized Hamburger remove(){
-        //if(!burgerTray.isEmpty()){
         return burgerTray.deQueue();
-        //}
-        //else{
-        //System.out.println("The tray is empty");
     }
     public void setMaxCap(int cap){
         this.trayMax = cap;
--- Oblig/Oblig-1/src/no/hvl/dat108/task2/dataobjects/Service.java
+++ Oblig/Oblig-1/src/no/hvl/dat108/task2/dataobjects/Service.java
@@ -1,17 +1,20 @@
 package no.hvl.dat108.task2.dataobjects;
 
 import no.hvl.dat108.task2.adt.Queue;
-public class Service {
+
+public class Service{
     private String name;
     private Queue<Hamburger> tray;
     public Service(String name, Queue<Hamburger> tray) {
         this.name = name;
         this.tray = tray;
     }//constructor
-    public void deliver(Hamburger burger) {
+    public synchronized void deliver(Hamburger burger) {
         //fjern øverste burger i fra køen
-        tray.deQueue();
-        System.out.println("Server " + name + " is serving a burger: " + burger.getId() + " traycount: " + tray.size());
+        if (!(tray.isEmpty() && tray.isNull())) {
+            tray.deQueue();
+            System.out.println("Server " + name + " is serving a burger: " + burger.getId() + " traycount: " + tray.size());
+        }
     }
     public String getName() {
         return name;
