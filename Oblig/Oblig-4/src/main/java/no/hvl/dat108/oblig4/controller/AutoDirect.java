6659860fa49e740548ea8bbfc976f8d8d0dde93a
h573763 <573763@stud.hvl.no>
2022-11-01 22:39:17 +0100

    Added autodirect controller class

--- Oblig/Oblig-4/src/main/java/no/hvl/dat108/oblig4/controller/AutoDirect.java
+++ Oblig/Oblig-4/src/main/java/no/hvl/dat108/oblig4/controller/AutoDirect.java
@@ -1,9 +1,14 @@
 package no.hvl.dat108.oblig4.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AutoDirect {
 
+    @GetMapping("/")
+    public String autoDirect(){
+        return "login";
+    }
 
 }
