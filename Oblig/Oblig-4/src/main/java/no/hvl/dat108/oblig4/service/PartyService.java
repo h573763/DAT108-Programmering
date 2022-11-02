4985d7768260ebb5eac500b7dccba01a62c3a97f
h573763 <573763@stud.hvl.no>
2022-11-02 16:01:06 +0100

    Added a Service class and Uility package

---
+++ Oblig/Oblig-4/src/main/java/no/hvl/dat108/oblig4/service/PartyService.java
@@ -0,0 +1,20 @@
+package no.hvl.dat108.oblig4.service;
+
+import no.hvl.dat108.oblig4.dataobjects.Person;
+import no.hvl.dat108.oblig4.repositories.PersonRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PartyService {
+
+    @Autowired
+    private PersonRepo personRepo;
+
+    public List<Person> findAllParticipants(int phonenumber){
+        return personRepo.findAllByPhonenumber(phonenumber);
+    }
+}
---
+++ Oblig/Oblig-4/src/main/java/no/hvl/dat108/oblig4/utilites/Validator.java
@@ -0,0 +1,4 @@
+package no.hvl.dat108.oblig4.utilites;
+
+public class Validator {
+}
