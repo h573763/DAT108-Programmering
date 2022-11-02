74def6427392152633f3b4ce9e0c6f26fdb6c0af
h573763 <573763@stud.hvl.no>
2022-11-02 15:59:39 +0100

    PersonRepo added

---
+++ Oblig/Oblig-4/src/main/java/no/hvl/dat108/oblig4/repositories/PersonRepo.java
@@ -0,0 +1,26 @@
+package no.hvl.dat108.oblig4.repositories;
+
+import no.hvl.dat108.oblig4.dataobjects.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PersonRepo extends JpaRepository<Person, Integer> {
+
+    /**
+     * To use for the list.jsp
+     * @return A list of participants
+     */
+    List<Person> findAllByPhonenumber(int phonenumber);
+
+    /**
+     * To use in the validator class if the phonenumber and password is connected to the same entity
+     * @param phonenumber
+     * @param password
+     * @return The person the phonenumber and password belongs to
+     */
+
+    Person findByPhonenumberAndPassword(int phonenumber, String password);
+}
