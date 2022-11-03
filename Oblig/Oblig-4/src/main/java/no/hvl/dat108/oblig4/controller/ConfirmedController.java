6df8c31802273c9a111ff40df2e0383886a18d67
h573763 <573763@stud.hvl.no>
2022-11-03 15:17:57 +0100

    Added ConfirmedController for the jsp file

---
+++ Oblig/Oblig-4/src/main/java/no/hvl/dat108/oblig4/controller/ConfirmedController.java
@@ -0,0 +1,15 @@
+package no.hvl.dat108.oblig4.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/confirmed")
+public class ConfirmedController {
+
+    @GetMapping
+    public String pageView(){
+        return "confirmed";
+    }
+}
