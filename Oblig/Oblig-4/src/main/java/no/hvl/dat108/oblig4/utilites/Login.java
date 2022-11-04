684888423de3ad421a9317a16cf43ba131434c63
h573763 <573763@stud.hvl.no>
2022-11-04 14:06:51 +0100

    Login implementation

--- Oblig/Oblig-4/src/main/java/no/hvl/dat108/oblig4/utilites/Login.java
+++ Oblig/Oblig-4/src/main/java/no/hvl/dat108/oblig4/utilites/Login.java
@@ -1,4 +1,28 @@
 package no.hvl.dat108.oblig4.utilites;
 
+import no.hvl.dat108.oblig4.dataobjects.Person;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 public class Login {
-}
+
+        private static final int MAX_INTERVAL = 90;
+        public static void userLogOut(HttpSession session){
+            session.invalidate();
+        }
+        public static void userLogIn(HttpServletRequest request, String password, String phonenumber){
+            userLogOut(request.getSession());
+
+            HttpSession session = request.getSession();
+            session.setMaxInactiveInterval(MAX_INTERVAL);
+            session.setAttribute("password", password);
+            session.setAttribute("phonenumber", phonenumber);
+            session.setAttribute("person", new Person());
+        }
+        public static boolean isNotLogedIn(HttpSession session){
+            return session == null
+                    || session.getAttribute("password") == null
+                    || session.getAttribute("list") == null;
+        }
+    }
--- Oblig/Oblig-4/src/main/java/no/hvl/dat108/oblig4/utilites/Validator.java
+++ Oblig/Oblig-4/src/main/java/no/hvl/dat108/oblig4/utilites/Validator.java
@@ -1,4 +1,23 @@
 package no.hvl.dat108.oblig4.utilites;
 
+import no.hvl.dat108.oblig4.dataobjects.Person;
+import no.hvl.dat108.oblig4.service.PartyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Validator {
+
+    @Autowired
+    PartyService ps;
+
+    public boolean isValid(String password, String phonenumber){
+        int number = Integer.parseInt(phonenumber);
+        for(Person person : ps.findAllParticipants()){
+            if(person.getPhonenumber() == number){
+                return person.getPassword().equals(password);
+            }
+        }
+        return false;
+    }
 }
