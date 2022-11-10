90eecbff22f578d15a56869ae63736c319277534
h573763 <573763@stud.hvl.no>
2022-11-10 17:01:50 +0100

    Seperated code from isValid to checker

--- Oblig/Oblig-4/src/main/java/no/hvl/dat108/oblig4/utilites/Validator.java
+++ Oblig/Oblig-4/src/main/java/no/hvl/dat108/oblig4/utilites/Validator.java
@@ -2,11 +2,13 @@ package no.hvl.dat108.oblig4.utilites;
 
 import no.hvl.dat108.oblig4.dataobjects.Person;
 import no.hvl.dat108.oblig4.service.PartyService;
+import org.hibernate.type.NumericBooleanType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 @Component
 public class Validator {
@@ -15,10 +17,19 @@ public class Validator {
     PartyService ps;
 
     public boolean isValid(String password, String phonenumber) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+       return checker(password, phonenumber, ps.findAllParticipants());
+    }
+    public boolean checker(String password, String phonenumber, List<Person> persons) throws UnsupportedEncodingException, NoSuchAlgorithmException{
         String hash;
+        int number = 0;
 
-        int number = Integer.parseInt(phonenumber);
-        for(Person person : ps.findAllParticipants()){
+        try {
+            number = Integer.parseInt(phonenumber);
+        }catch (NumberFormatException e){
+            e.printStackTrace();
+        }
+
+        for(Person person : persons){
             if(person.getPhonenumber() == number){
                 hash = HashPassword.hash(password, person.getSalt());
                 return person.getHash().equals(hash);
